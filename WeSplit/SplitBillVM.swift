38ad207dc48a14d92ab2a2b208e7dc996bbe6a4b
george.eccleston@me.com <george.eccleston@me.com>
2024-01-21 21:32:45 -0700

    change people selection to drop down list

--- WeSplit/SplitBillVM.swift
+++ WeSplit/SplitBillVM.swift
@@ -9,11 +9,12 @@ import Foundation
 
 class SplitBillVM: Observable {
     
-    let peopleCount  = [2, 3, 4, 5, 6, 7, 8, 9, 10]
-    let tipSelection = [0, 1, 3, 5, 10, 15, 20, 25]
+    let peopleCount = [2, 3, 4, 5, 6, 7, 8, 9, 10]
+    let tipPerCent  = [0, 1, 3, 5, 10, 15, 20, 25]
     
     init() {
-        
+//        self.peopleCount = peopleCount
+//        self.tipPerCent = tipPerCent
     }
 
 }
--- WeSplit/SplitBillView.swift
+++ WeSplit/SplitBillView.swift
@@ -6,45 +6,43 @@
 import SwiftUI
 
 struct SplitBillView: View {
-    @State private var billAmount: Double = 0
+    @State private var billAmount:   Double = 0.0
     @State private var numberPeople: Int = 2
-    @State private var tipPercent: Int = 10
+    @State private var tipPercent:   Int = 10
     
-    @State private var tipAmount: Double = 0
-    @State private var billWithTip: Double = 0
-    @State private var personAmount: Double = 0
+    @State private var tipAmount:    Double = 0.0
+    @State private var billWithTip:  Double = 0.0change people selection to drop down list
+    @State private var personAmount: Double = 0.0
 
     
     var body: some View {
         
         let splitBillCalculation = SplitBillVM()
 
-        
     NavigationStack {
             Form {
                 Section("Enter Bill Amount") {
                     TextField("Enter Bill Amount", value: $billAmount, format: .number)
-
+                        .keyboardType(.default)
                         .fontWeight(.heavy)
                 }
-                
-                Section("Select Number of People") {
-                    Picker("People Count", selection: $numberPeople) {
-                        ForEach(splitBillCalculation.peopleCount, id: \.self) {
-                            Text($0.formatted())
-                                .fontWeight(.heavy)
+                    Section("Select Number of People") {
+                        Picker("People Count", selection: $numberPeople) {
+                            ForEach(splitBillCalculation.peopleCount, id: \.self) {
+                                Text($0.formatted())
+                                    .fontWeight(.heavy)
+                            }
                         }
                     }
-                    .pickerStyle(.segmented)
-                }
                 
                 Section("Select tip percentage") {
                     Picker("TipPercentage", selection: $tipPercent) {
-                        ForEach(splitBillCalculation.tipSelection, id: \.self) {
+                        ForEach(splitBillCalculation.tipPerCent, id: \.self) {
                             Text($0, format: .percent)
                         }
                     }
                     .pickerStyle(.segmented)
+                    
                     HStack {
                         Text("Selected Tip Amount = ")
                         if tipPercent != 0 {
@@ -57,18 +55,19 @@ struct SplitBillView: View {
                             .foregroundColor(Color.red)
                         }
                     }
-                    HStack {
-                        if tipPercent != 0 {
-                            Text("Bill plus Tip = ")
-                        } else {
-                            Text("Bill with no Tip = ")
-                                .fontWeight(.heavy)
-                                .foregroundColor(Color.red)
-                        }
-                        Text(billAmount * (1.0 + Double(tipPercent)/100), format: .number)
-                            .fontWeight(.heavy)
-                            .foregroundColor(Color.blue)
-                    }
+                    
+//                    HStack {
+//                        if tipPercent != 0 {
+//                            Text("Bill plus Tip = ")
+//                        } else {
+//                            Text("Bill with no Tip = ")
+//                                .fontWeight(.heavy)
+//                                .foregroundColor(Color.red)
+//                        }
+//                        Text(billAmount * (1.0 + Double(tipPercent)/100), format: .number)
+//                            .fontWeight(.heavy)
+//                            .foregroundColor(Color.blue)
+//                    }
                 }
 
                 Section("Amount for each person") {
